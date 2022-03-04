b61a3a8305c67f369d5fc9ed2664739690db81a0
nidhipawar5 <nidhipawar0504@gmail.com>
2022-03-05 03:23:06 +0530

    pizza store listing app

---
+++ src/components/Footer/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./styles.module.css";
+
+
+function Footer() {
+  
+
+  return (
+    <div className="footer_container">
+      <footer>
+        <h3>
+          <p>For further queries contact : (+91)9876543210</p>
+          <p>Store timings: 11am-10pm</p>
+        </h3>
+        
+        
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
